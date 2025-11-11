97437966f7465295dddf9a95f8497685ad5e099c
rajapriya118 <44433754+rajapriya118@users.noreply.github.com>
2025-11-12 00:08:33 +0530

    Update hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -3,11 +3,11 @@ const { Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
 setDefaultTimeout(120000);
 
 Before(async function () {
-  console.log('🌐 [Before Hook] Launching browser...');
+  console.log('[Before Hook] Launching browser...');
   await this.launchBrowser({ headless: false }); // switch to true for CI/CD
 });
 
 After(async function () {
-  console.log('🧹 [After Hook] Closing browser...');
+  console.log('[After Hook] Closing browser...');
   await this.closeBrowser();
 });
