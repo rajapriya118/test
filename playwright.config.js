793bc6b4c116790d1442d4d4ae11c3a51bbbd6d3
rajapriya <rajapriya@surfaceinsight.com>
2025-11-11 23:34:03 +0530

    src added

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-// Minimal Playwright config to use Chromium headless
-module.exports = {
-  use: {
-    browserName: 'chromium',
-    headless: true,
-    screenshot: 'only-on-failure',
-    trace: 'retain-on-failure'
-  }
-};
